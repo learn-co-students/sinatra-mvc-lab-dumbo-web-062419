913e2624ce1b7cffbef5fd6357c6d94a728de7a8
lgm527 <laurellmccaffrey@gmail.com>
2019-07-15 19:38:04 -0400

    pig latin translated

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+     @new = PigLatinizer.new
+     @pig_text = @new.piglatinize(params[:user_phrase])
+    erb :pig_input
+  end
 end
